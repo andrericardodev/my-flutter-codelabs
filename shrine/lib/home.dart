a31cff84a57296d99445306142dda4c14bdb7f79
Andre Ricardo <andrericardo.developer@outlook.com>
2024-11-09 14:04:22 -0300

    layout home

--- shrine/lib/home.dart
+++ shrine/lib/home.dart
@@ -1,14 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:shrine/model/product.dart';
+import 'package:shrine/model/products_repository.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product) {
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AspectRatio(
+              aspectRatio: 18.0 / 11.0,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      product.name,
+                      style: theme.textTheme.titleLarge,
+                      maxLines: 1,
+                    ),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.titleSmall,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    }).toList();
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Scaffold(
-        body: Center(
-          child: Text('HomePage'),
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(
+              Icons.menu,
+              semanticLabel: 'Menu',
+            ),
+            onPressed: () {
+              print('Menu button');
+            },
+          ),
+          title: const Text('SHRINE'),
+          actions: [
+            IconButton(
+              icon: const Icon(
+                Icons.search,
+                semanticLabel: 'search',
+              ),
+              onPressed: () {
+                print('Search button');
+              },
+            ),
+            IconButton(
+              icon: const Icon(
+                Icons.tune,
+                semanticLabel: 'filter',
+              ),
+              onPressed: () {
+                print('Filter button');
+              },
+            ),
+          ],
+        ),
+        body: GridView.count(
+          crossAxisCount: 2,
+          padding: const EdgeInsets.all(16.0),
+          childAspectRatio: 8.0 / 9.0,
+          children: _buildGridCards(context),
         ),
         resizeToAvoidBottomInset: false,
       ),
